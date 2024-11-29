517e43c43b03f7aeea8e225382a7c56de5d5eee6
Documenter.jl <documenter@juliadocs.github.io>
2024-11-29 04:20:48 +0000

    build based on 450b9fe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TextPieces","category":"page"},{"location":"#TextPieces","page":"Home","title":"TextPieces","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TextPieces.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TextPieces]","category":"page"}]
+}
